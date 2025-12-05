ffd4bf12f91b7460e552fe730942b7a3e7bcd670
beatrizcamilo07 <beatriz.camilo@esolar.ifrn.edu.br>
2025-12-05 19:11:41 -0300

    login-mvc

--- login/protect.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(!isset($_SESSION)){
-    session_start();
-}
-if(!isset($_SESSION['id'])){
-    die("voce não pode acessar a pagina, loge. <p><a href=\"index.php\">Entrar</a></p>");
-}
-?>
